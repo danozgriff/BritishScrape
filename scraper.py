0ccc8585fc358eab03442317ff459cf3b61104f1
danozgriff <dane.griff@gmail.com>
2016-02-23 21:06:30 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ import datetime
 
 #scraperwiki.sqlite.execute("create table Company_Performance (`TIDM` varchar2(8) NOT NULL, `1D` real, `3D` real, `1W` real, '1M' real, '6M' real,  `Date` date, UNIQUE (`TIDM`, `Date`))")
  
-if 1==0: 
+if 1==1: 
  
    complist = scraperwiki.sqlite.execute("select `TIDM`, `Price` from company where TIDM in (select distinct TIDM from Signal_History)")
         
