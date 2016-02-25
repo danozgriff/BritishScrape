54d0b3c08ae3db123a794ee99d665bc1db9b45c5
danozgriff <dane.griff@gmail.com>
2016-02-25 21:18:13 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ import datetime
 
 #scraperwiki.sqlite.execute("create table Company_Performance (`TIDM` varchar2(8) NOT NULL, `1D` real, `3D` real, `1W` real, '1M' real, '6M' real,  `Date` date, UNIQUE (`TIDM`, `Date`))")
  
-if 1==0: 
+if 1==1: 
  
    complist = scraperwiki.sqlite.execute("select `TIDM`, `Price` from company where TIDM in (select distinct TIDM from Signal_History)")
 
@@ -26,18 +26,18 @@ if 1==0:
 #D-1
 
        d1date=ndate - datetime.timedelta(days=10)
-       d1list = scraperwiki.sqlite.execute("select `Price` from Signal_History where tidm = '" + tidm + "' and 	strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%m', date) = '" + str(d1date.month) + "' and	strftime('%d', date) = '" + str(d1date.day) + "'")
+       d1list = scraperwiki.sqlite.execute("select `Price` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%-m', date) = '" + str(d1date.month) + "' and	strftime('%-d', date) = '" + str(d1date.day) + "'")
 
        for y in d1list["data"]:
            print y[0]
-           #break
-       #break
+           break
+       break
        
-if 1==1:
+if 1==0:
      tidm = "test"
      ndate=datetime.date.today()
      d1date=ndate - datetime.timedelta(days=10)
-     print "select `Price` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%mm', date) = '" + str(d1date.month) + "' and	strftime('%dd', date) = '" + str(d1date.day) + "'"
+     print "select `Price` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%-m', date) = '" + str(d1date.month) + "' and	strftime('%-d', date) = '" + str(d1date.day) + "'"
      #ndate=datetime.date.today()
      #print ndate.year
      #strftime('%Y', ndate)
