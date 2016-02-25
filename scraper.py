ab3ba502edfbad24bf12de4cd4bef3935f27e0b5
danozgriff <dane.griff@gmail.com>
2016-02-25 21:37:08 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@ if 1==1:
 
        d1date=ndate - datetime.timedelta(days=10)
        #d1list = scraperwiki.sqlite.execute("select `Price` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%-m', date) = '" + str(d1date.month) + "' and	strftime('%-d', date) = '" + str(d1date.day) + "'")
-       d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "'")
+       d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = 2016")
 
        for y in d1list["data"]:
            print y[0]
