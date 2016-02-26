1df30a38bfe6d9a466cf3b954d851fe18ae257a5
danozgriff <dane.griff@gmail.com>
2016-02-26 10:28:42 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ import time
 
 #scraperwiki.sqlite.execute("create table Company_Performance (`TIDM` varchar2(8) NOT NULL, `1D` real, `3D` real, `1W` real, '1M' real, '6M' real,  `Date` date, UNIQUE (`TIDM`, `Date`))")
  
-if 1==0: 
+if 1==1: 
  
    complist = scraperwiki.sqlite.execute("select `TIDM`, `Price` from company where TIDM in (select distinct TIDM from Signal_History)")
 
@@ -30,7 +30,8 @@ if 1==0:
        #d1list = scraperwiki.sqlite.execute("select `Price` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%-m', date) = '" + str(d1date.month) + "' and	strftime('%-d', date) = '" + str(d1date.day) + "'")
        #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and CAST(strftime('%m', Datde) AS INTEGER) > 1")
        #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y-%m-%d',Date) = '2016-02-15'")
-       d1list = scraperwiki.sqlite.execute("""select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date)= ?""", (str(d1date.month),) ) # and Date = Datetime('15/2/2016')")
+       d1list = scraperwiki.sqlite.execute("""select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%%Y',date,'unixepoch')= '%s'"""%(str(d1date.year))
+       
        for y in d1list["data"]:
            print y[0]
            print y[1]
@@ -38,12 +39,12 @@ if 1==0:
        break    
        
        
-if 1==1:
+if 1==0:
      tidm = "test"
      ndate=datetime.date.today()
      d1date=ndate - datetime.timedelta(days=10)
      print d1date
-     print "select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date)= ?", str(d1date.month)
+     print "select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date)= ?", str(d1date.year)
      #ndate=datetime.date.today()
      #print ndate.year
      #strftime('%Y', ndate)
