d8f37649bc08d7775d2ecd629c6e69b1b48807cc
danozgriff <dane.griff@gmail.com>
2016-02-26 09:56:40 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -30,11 +30,12 @@ if 1==1:
        #d1list = scraperwiki.sqlite.execute("select `Price` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date) = '" + str(d1date.year) + "' and	strftime('%-m', date) = '" + str(d1date.month) + "' and	strftime('%-d', date) = '" + str(d1date.day) + "'")
        #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and CAST(strftime('%m', Datde) AS INTEGER) > 1")
        #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y-%m-%d',Date) = '2016-02-15'")
-       d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and Date = Datetime('15/2/2016')")
+       d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date`, strftime('%Y', `Date`) from Signal_History where tidm = '" + tidm + "'") # and Date = Datetime('15/2/2016')")
        for y in d1list["data"]:
            print y[0]
            print y[1]
            print y[2]
+           print y[3]
        break    
        
        
