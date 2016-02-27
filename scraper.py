349d5358f029ad7a7b5009313515be09e978a22c
danozgriff <dane.griff@gmail.com>
2016-02-27 21:06:22 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -40,8 +40,8 @@ if 1==1:
        #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and Date = '%d'" % (d1date))
        print  "%s-%s-%s" % (d1date.strftime('%Y'), d1date.strftime('%m'), d1date.strftime('%d'))
        print " "
-       #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Date`, strftime('%m',Date,'unixepoch') from Signal_History where tidm = '" + tidm + "' and Date < '%s-%s-%s'" % (d1date.strftime('%Y'), d1date.strftime('%m'), d1date.strftime('%d')))
-       d1list = scraperwiki.sqlite.execute("select `TIDM`, `Date`, strftime('%m',SH.Date) from Signal_History as SH where tidm = '" + tidm + "' and SH.Date > '2015-01-15'") # '%s-%s-%s' group by `TIDM`" % (d1date.year, d1date.month, d1date.day))
+       d1list = scraperwiki.sqlite.execute("select `TIDM`, `Date`, strftime('%m',Date,'unixepoch') from Signal_History where tidm = '" + tidm + "' and Date > '%s-%s-%s'" % (d1date.year, d1date.month, d1date.day))
+       #d1list = scraperwiki.sqlite.execute("select `TIDM`, `Date`, strftime('%m',SH.Date) from Signal_History as SH where tidm = '" + tidm + "' and SH.Date > '2015-01-15'") # '%s-%s-%s' group by `TIDM`" % (d1date.year, d1date.month, d1date.day))
        for y in d1list["data"]:
            print y[0]
            print y[1]
