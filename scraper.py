2c220318815647677b32d2c4339e476586bb48bb
danozgriff <dane.griff@gmail.com>
2016-02-27 16:19:43 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ import time
 
 #scraperwiki.sqlite.execute("create table Company_Performance (`TIDM` varchar2(8) NOT NULL, `1D` real, `3D` real, `1W` real, '1M' real, '6M' real,  `Date` date, UNIQUE (`TIDM`, `Date`))")
  
-if 1==1: 
+if 1==0: 
  
    complist = scraperwiki.sqlite.execute("select `TIDM`, `Price` from company where TIDM in (select distinct TIDM from Signal_History)")
 
@@ -48,12 +48,12 @@ if 1==1:
        break   
        
        
-if 1==0:
+if 1==1:
      tidm = "test"
      ndate=datetime.date.today()
      d1date=ndate - datetime.timedelta(days=10)
      print d1date
-     print "select `TIDM`, `Price`, `Date` from Signal_History where tidm = '" + tidm + "' and strftime('%Y', date)= ?", str(d1date.year)
+     print ""select `TIDM`, `Date`, strftime('%m',Date,'unixepoch') from Signal_History where tidm = '" + tidm + "' and Date < '%s-%s-%s' group by `TIDM`" % (d1date.year, d1date.month, d1date.day)
      #ndate=datetime.date.today()
      #print ndate.year
      #strftime('%Y', ndate)
