ae65e7abdb80342937221f7aad4aa664513c96b1
danozgriff <dane.griff@gmail.com>
2016-02-28 12:53:33 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -72,7 +72,7 @@ if 1==1:
            if Abovedelta.days > Belowdelta.days:
                MinMaxDelta = MaxDate - MinDate
                PriceDelta = MaxPrice - MinPrice
-               PriceInterval = PriceDelta / float(str(MinMaxDelta))
+               PriceInterval = PriceDelta / MinMaxDelta.days
                CalcPrice = nprice+Belowdelta*PriceInterval
                print CalcPrice
            #print 'Delta: %d' % (delta.days)
