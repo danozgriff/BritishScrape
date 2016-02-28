c9e83585ba88db5c1e5b297943ca11f9551ffb72
danozgriff <dane.griff@gmail.com>
2016-02-28 09:54:39 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -39,8 +39,8 @@ if 1==1:
        if len(d1list["data"]) == 0:
            d1maxdate = scraperwiki.sqlite.execute("select max(`Date`) as MaxDate from Signal_History where tidm = '%s' and Date < '%s'" % (tidm, d1date))
            print "Value : %s" %  d1maxdate.keys()
-           
-           #print "Value : %s" %  d1maxdate.get('MaxDate')
+           print d1maxdate["data"]
+           print "Value : %s" %  d1maxdate.get('data')
 
            #for y in d1maxdate["data"]:
                #print y[0]
